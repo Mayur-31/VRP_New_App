b190413a1dddfe861cef847181b71352ca4bca15
Mayur Shinde <90058629+Mayur-31@users.noreply.github.com>
2025-06-07 13:38:09 +0530

    Delete VRP_new_app/kamal.ps1

--- VRP_new_app/kamal.ps1
+++
@@ -1,9 +0,0 @@
-function kamal {
-    docker run -it --rm `
-    -v "${PWD}:/workdir" `
-    -e "DOCKER_HOST=tcp://host.docker.internal:2375" `
-    -e "KAMAL_REGISTRY_PASSWORD=$env:KAMAL_REGISTRY_PASSWORD" `
-    -e "OPENCAGE_API_KEY=$env:OPENCAGE_API_KEY" `
-    --workdir /workdir `
-    ghcr.io/basecamp/kamal:latest @args
-}
